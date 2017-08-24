b78567d89a09f5293c09ca74822e3fa31e2e2b44
Jason Sturges <jason@jasonsturges.com>
2017-08-23 22:59:57 -0500

    PostgreSQL database context

--- PostgreSqlDotnetCore/Startup.cs
+++ PostgreSqlDotnetCore/Startup.cs
@@ -27,7 +27,7 @@ namespace PostgreSqlDotnetCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
