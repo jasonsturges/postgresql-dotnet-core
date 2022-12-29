700312cf33be653431d1b875b5150ead58423ca7
Jason Sturges <jason@jasonsturges.com>
2022-12-29 14:23:32 -0600

    Update Program.cs to use Npgsql db context

--- PostgreSqlDotnetCore/Program.cs
+++ PostgreSqlDotnetCore/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+    options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
